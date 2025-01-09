bee37480da044a4b8931e4a2d344c7aa9c8ed13d
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-01-09 21:55:31 +0400

    Add some tests for collections and stacks

---
+++ test/collections.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from '@jest/globals';
+import { sdk } from './sdk';
+
+describe('Collections', () => {
+    it('should create collection', async () => {
+        const collection = await sdk.collections.create();
+        expect(collection).toBeDefined();
+
+        const sameCollection = await sdk.collections.get({ id: collection.id });
+        expect(sameCollection).toEqual(collection);
+    });
+});
---
+++ test/stacks.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from '@jest/globals';
+import { sdk } from './sdk';
+
+describe('Stacks', () => {
+    it('should create stack', async () => {
+        const stack = await sdk.stacks.create();
+        expect(stack).toBeDefined();
+
+        const sameStack = await sdk.stacks.get({ id: stack.id });
+        expect(sameStack).toEqual(stack);
+    });
+});
