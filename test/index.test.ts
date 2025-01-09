b10d1ea6f0ee30ba670cb042510044562ddbaef2
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-01-09 21:25:44 +0400

    Add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,14 @@
+import { createClientSDK } from '../src';
 import { describe, it, expect } from '@jest/globals';
 
 describe('Client SDK', () => {
-    it('should work', async () => {});
+    it('should get balance', async () => {
+        const client = createClientSDK({
+            authToken: () => process.env.SDK_TOKEN!,
+            endpoint: process.env.API_CORE_URL!,
+        });
+
+        const balance = await client.balances.get();
+        expect(balance).toBeDefined();
+    });
 });
