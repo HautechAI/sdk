93af9da511a36f263d3da4bb0f4276164d084ec3
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-01-09 21:51:23 +0400

    Add accounts test

---
+++ test/accounts.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from '@jest/globals';
+import { sdk } from './sdk';
+
+describe('Balances', () => {
+    it('should create account', async () => {
+        const alias = Math.random().toString(36).substring(2, 15);
+
+        const account = await sdk.accounts.create({ alias });
+        expect(account).toBeDefined();
+
+        const sameAccount = await sdk.accounts.get({ id: account.id });
+        expect(sameAccount).toEqual(account);
+
+        const sameAccountByAlias = await sdk.accounts.getByAlias({ alias });
+        expect(sameAccountByAlias).toEqual(account);
+    });
+});
