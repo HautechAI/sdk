30915100f9dbf27cbdbf35a3fb09ed2774b359e4
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-01-10 19:57:44 +0400

    Add more storage tests

--- test/storage.test.ts
+++ test/storage.test.ts
@@ -20,4 +20,33 @@ describe('Storage', () => {
         const records = await sdk.storage.getMany({ keys: [key] });
         expect(records).toEqual({ [key]: value });
     });
+
+    it('should update record', async () => {
+        const key = randomString();
+        const value = 'test';
+        const updatingValue = 'test2';
+
+        const record = await sdk.storage.create({ key, value });
+        expect(record).toBeDefined();
+
+        const records = await sdk.storage.getMany({ keys: [key] });
+        expect(records).toEqual({ [key]: value });
+
+        const record2 = await sdk.storage.update({ key, value: updatingValue });
+        expect(record2).toBeDefined();
+
+        const records2 = await sdk.storage.getMany({ keys: [key] });
+        expect(records2).toEqual({ [key]: updatingValue });
+    });
+
+    it('should delete record', async () => {
+        const key = randomString();
+        const value = 'test';
+
+        await sdk.storage.create({ key, value });
+        await sdk.storage.delete({ key });
+
+        const records = await sdk.storage.getMany({ keys: [key] });
+        expect(records).toEqual({});
+    });
 });
