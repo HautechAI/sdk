7d6729d407d7d4117cdbdd4d00c5395397d1c110
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-01-27 22:18:59 +0400

    Fix permissions

--- src/token/permissions.ts
+++ src/token/permissions.ts
@@ -18,22 +18,3 @@ export const defaultPermissions: MethodsPermissions = {
     stacks: { create: true, items: { add: true, remove: true }, read: true, metadata: { update: true } },
     storage: { create: true, delete: true, read: true, update: true },
 };
-
-export const defaultRootPermissions: MethodsPermissions = {
-    access: { add: true, read: true, remove: true },
-    accounts: { create: true, read: true },
-    balances: { read: true, self: { read: true, update: true }, update: true },
-    collections: {
-        create: true,
-        items: { add: true, read: true, remove: true },
-        read: true,
-        update: true,
-    },
-    groups: { accounts: { add: true, read: true, remove: true }, create: true, delete: true, read: true },
-    images: { create: true, read: true },
-    operations: { create: true, read: true, metadata: { update: true } },
-    pipelines: { create: true, read: true, update: true },
-    poses: { create: true, read: true },
-    stacks: { create: true, items: { add: true, remove: true }, read: true, metadata: { update: true } },
-    storage: { create: true, delete: true, read: true, update: true },
-};
